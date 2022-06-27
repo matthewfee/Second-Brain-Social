9583bb087f9d3afb1118f58607ef73a8f5eddd7a
Saka Ricky <sakaricky91@gmail.com>
2022-06-27 13:47:11 +0100

    feat: refactored user signup api

--- services/users.js
+++ services/users.js
@@ -1,19 +1,54 @@
-import { collection, addDoc, doc, deleteDoc, onSnapshot } from 'firebase/firestore';
-import { db } from './firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { collection, addDoc, doc, deleteDoc, onSnapshot, setDoc, getDoc } from 'firebase/firestore';
+import { db, auth } from './firebase';
 
 // collection ref
 const usersColRef = collection(db, 'users');
+
+// get 1 user from db
+export const getUser = async (uid) => {
+  try {
+    const userDocRef = doc(db, 'users', uid);
+    const docSnap = await getDoc(userDocRef);
+
+    if (docSnap.exists()) {
+      console.log('user data:', docSnap.data());
+    } else {
+      // doc.data() will be undefined in this case
+      console.log('No such document!');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // get collection data
 export const getUsers = async () => {
   try {
     onSnapshot(usersColRef, (snapshot) => {
       const users = snapshot.docs.map((snapshotDoc) => snapshotDoc.data());
-      console.log(users);
-      //   return cityList;
+      console.log('users in services: ', users);
+      return users;
     });
   } catch (error) {
     console.log(error);
+  }
+};
+
+// create a user
+export const createUser = async (user) => {
+  try {
+    const userData = await createUserWithEmailAndPassword(auth, user.email, user.password);
+    await setDoc(doc(db, 'users', userData.user.uid), {
+      name: user.name,
+      dateOfBirth: user.dateOfBirth,
+      gender: user.gender,
+    });
+
+    return userData.user;
+  } catch (error) {
+    console.log(error.message);
+    return error.message;
   }
 };
 
