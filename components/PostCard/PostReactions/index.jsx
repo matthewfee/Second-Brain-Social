4b1578158471a09080480d2d8caae6a6884a3902
Matt <60991158+matthewfee@users.noreply.github.com>
2022-07-30 18:12:57 +0200

    removed share button

--- components/PostCard/PostReactions/index.jsx
+++ components/PostCard/PostReactions/index.jsx
@@ -75,7 +75,7 @@ export const PostReactions = ({ post, toggleShowComments, showCommentsButton, fe
           Comments
         </button>
       )}
-      <div className="flex gap-1 items-center hover:text-blue-500 cursor-pointer">
+      {/* <div className="flex gap-1 items-center hover:text-blue-500 cursor-pointer">
         <svg
           id="Layer_1"
           data-name="Layer 1"
@@ -88,7 +88,7 @@ export const PostReactions = ({ post, toggleShowComments, showCommentsButton, fe
           <path d="M490.57,427.47H190.49a21.43,21.43,0,0,1-15.15-36.59l94.35-94.35c-25.08-23-58.36-35.85-93.85-35.85-66.18,0-122.24,44.69-133.31,106.27A21.43,21.43,0,0,1,.09,361.24C23,108.89,232.53,106,234.64,106c61,0,119.27,22.51,163.18,62.43l77.59-77.58A21.43,21.43,0,0,1,512,106V406A21.43,21.43,0,0,1,490.57,427.47ZM242.24,384.61H469.13V157.71L413,213.85a20.79,20.79,0,0,1-15.68,6.27,21.4,21.4,0,0,1-15.35-7c-37-40.84-90.73-64.26-147.32-64.26-5.31,0-102.79,2.17-157.39,97.35a183.54,183.54,0,0,1,98.58-28.36c55.1,0,106.24,23.91,140.3,65.61a21.43,21.43,0,0,1-1.44,28.72Z" />
         </svg>
         Share
-      </div>
+      </div> */}
     </div>
   );
 };
